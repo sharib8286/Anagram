81ae96c26225da8a8c31f05812b2c3c18ddf6a62
sharib8286 <132230510+sharib8286@users.noreply.github.com>
2023-05-01 00:14:54 +0200

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,17 @@
+package practicse;
+
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String args[]){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter String1 :");
+        var str1=scan.nextLine();
+       // System.out.println(str1);
+
+        System.out.println("Enter String2 :");
+        var str2=scan.nextLine();
+        //System.out.println(str2);
+    }
+}
